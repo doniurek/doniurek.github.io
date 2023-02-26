b496b5d196ef212f91ed52cce280cae863f65791
doniurek <aldonaglebicka@gmail.com>
2023-02-27 00:44:38 +0100

    Fix site height issue on Firefox

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function setHeight() {
-  let wheight = window.innerHeight
+  let height = window.innerHeight
   let contentClasses = document.getElementsByClassName("content")
 
   for (let i =0; i < contentClasses.length; i++) {
-      contentClasses[i].style.height = wheight + 'px'
+      contentClasses[i].style.height = height + 'px'
   }
 }
+
+window.addEventListener("load", (event) => {
+  setHeight()
+})
 
 addEventListener('resize', setHeight)
 addEventListener('orientationchange', setHeight)
